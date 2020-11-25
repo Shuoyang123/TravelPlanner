decea81007768a5d7e4890e9dd5349d9b85df35f
Shuoyang123 <sy2873@columbia.edu>
2020-11-25 16:00:23 -0500

    add state management

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,24 @@ import MyTrip from './MyTrip.js';
 const { TabPane } = Tabs;
 
 class Home extends Component {
+  state = {
+    city: "",
+    startDate:  "",
+    endDate: "",
+  }
+
+  updateCity(city, startDate, endDate){
+    //search's responsibility
+  }
+
    render() {
        return (
          <Tabs defaultActiveKey="1">
             <TabPane tab="Search" key="1">
-              <Search />
+              <Search updateCity = {this.updateCity}/>
             </TabPane>
             <TabPane tab="My Trip" key="2">
-              <MyTrip />
+              <MyTrip startDate = {this.state.startDate} endDate = {this.state.endDate} city = {this.state.city}/>
             </TabPane>
             <TabPane tab="History" key="3">
               <History />
--- src/components/MyTrip.js
+++ src/components/MyTrip.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
 class MyTrip extends Component {
+  state = {
+    attractions: []
+  }
+
    render() {
        return (
            <div>
